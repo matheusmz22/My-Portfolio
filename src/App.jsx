69277ca7dca833f56a38913602d09ae10fd6ef51
Matheus <matheusmzucchi@gmail.com>
2025-08-26 18:05:16 -0300

    Implemented DarkMode logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar/Navbar";
-import { useState } from "react";
 import Hero from "./components/Hero/Hero";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(true);
   return (
     <>
       <div className="bg-gradient-to-br from-hero-light via-hero-medium to-body h-screen p-5">
-        <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-        <Hero isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+        <Navbar />
+        <Hero />
       </div>
       <div className="bg-body">asdsa</div>
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { SunIcon } from "@heroicons/react/16/solid";
 import { useMediaQuery } from "react-responsive";
 import CardNav from "./CardNav";
+import DarkMode from "./DarkMode";
 
 function Navbar({ isDarkMode, setIsDarkMode }) {
   const isMobile = useMediaQuery({ maxWidth: 767 }); // Up to 767px is mobile
@@ -73,37 +74,7 @@ function Navbar({ isDarkMode, setIsDarkMode }) {
                   className="cursor-pointer mt-2 hover:bg-navbarlight transition-all duration-100 rounded-[7px]"
                   onClick={() => setIsDarkMode(!isDarkMode)}
                 >
-                  {isDarkMode ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="size-8"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="size-8"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
-                      />
-                    </svg>
-                  )}
+                  <DarkMode />
                 </button>
               </li>
             </ul>
